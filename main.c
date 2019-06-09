24f77e2eb0d3b072c28115396348ee9f21f8e811
qypec <qypec.prog@yandex.ru>
2019-06-09 16:15:45 +0300

    added exit command

--- main.c
+++ main.c
@@ -9,9 +9,35 @@
 // 	return (0);
 // }
 
+int						is_exit(const char *buff)
+{
+	if (ft_strncmp(buff, "exit", 4) == 0 && buff[4] == '\0')
+		return (1);
+	return (0);
+}
+
+void					parse_command(const char *buff)
+{
+// 	char * env [] = {"HOME = / usr / home", "LOGNAME = home", (char *) 0};
+// 	char * cmd [] = {"ls", (char *) 0};
+// 	pid_t			p;
+
+// 	p = fork();
+// 	if ((p = fork()) == 0)
+// 		execve("/bin/pwd", cmd, envv);
+// 	else
+// 		wait(NULL);
+	if (is_exit(buff) == 1)
+	{
+		write(1, "See you!\n", 9);
+		exit(0);
+	}
+}
+
 void 					handle_sigint(int sig) 
 { 
-    printf("Caught signal %d\n", sig);
+    // printf("Caught signal %d\n", sig);
+    write(1, "\n", 1);
     display_prompt();
 } 
   
@@ -25,8 +51,7 @@ void					get_input(void)
 	int			buff_counter;
 
 	i = 0;
-	// char * env [] = {"HOME = / usr / home", "LOGNAME = home", (char *) 0};
-	// char * cmd [] = {"ls", (char *) 0};
+
 	signal(SIGINT, handle_sigint);
 	buff = (char *)ft_memalloc(sizeof(char) * BUFF_SIZE);
 	buff_counter = BUFF_SIZE;
@@ -34,7 +59,7 @@ void					get_input(void)
 	{
 		if (symb == '\n')
 		{
-			printf("break\n");
+			// write(1, "break\n", 6);
 			break ;
 		}
 		if (buff_counter <= i)
@@ -44,17 +69,7 @@ void					get_input(void)
 		}
 		buff[i++] = symb;
 	}
-
-	// pid_t p;
-
-	// printf("process: pid = %d\n ppid = %d\n", getpid(), getppid());
-	// p = fork();
-	// if (buff[0] == 'l' && buff[1] == 's')
-	// 	execve("/bin/ls", cmd, env);
-	// printf("process: pid = %d\n ppid = %d\n", getpid(), getppid());
-	// printf("hello\n");
-	// printf("buff = %s\n", buff);
-	// parse_input(&buff);
+	parse_command(buff);
 	ft_strdel(&buff);
 }
 
@@ -75,18 +90,7 @@ int							main(int argc, char **argv, char **envv)
 	// pid_t p;
 
  //   	printf("process: pid = %d ppid = %d\n\n\n", getpid(), getppid());
-	// if ((p = fork()) == 0)
-	// {
-	// 	printf("process: p = %d\n", p);
-	// 	printf("process: pid = %d ppid = %d\n\n", getpid(), getppid());
-	// 	execve("/bin/pwd", cmd, envv);
-	// }
-	// else
-	// {
-	// 	wait(NULL);
-	// 	printf("process: p = %d\n", p);
-	// 	printf("process: pid = %d ppid = %d\n", getpid(), getppid());
-	// }
+
 	// display_prompt();
 
     return (0);
